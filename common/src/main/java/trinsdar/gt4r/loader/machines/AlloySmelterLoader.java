5ba68df14ec5b9770e64f35b3fac54f019e04e38
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-01-14 19:00:49 -0500

    improved and deduplicated alloy smelter recipes
    also added recipe for leaded redstone

--- common/src/main/java/trinsdar/gt4r/loader/machines/AlloySmelterLoader.java
+++ common/src/main/java/trinsdar/gt4r/loader/machines/AlloySmelterLoader.java
@@ -1,11 +1,19 @@
 package trinsdar.gt4r.loader.machines;
 
+import com.google.common.collect.ImmutableMap;
 import muramasa.antimatter.data.AntimatterMaterialTypes;
 import muramasa.antimatter.data.AntimatterMaterials;
 import muramasa.antimatter.material.Material;
 import muramasa.antimatter.recipe.ingredient.RecipeIngredient;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
 import org.gtreimagined.gtcore.data.GTCoreItems;
+import org.gtreimagined.gtcore.data.GTCoreMaterials;
 import trinsdar.gt4r.data.GT4RMaterialTags;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static muramasa.antimatter.data.AntimatterMaterialTypes.*;
 import static muramasa.antimatter.data.AntimatterMaterials.*;
@@ -15,20 +23,18 @@ import static trinsdar.gt4r.data.RecipeMaps.ALLOY_SMELTER;
 
 public class AlloySmelterLoader {
     public static void init(){
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), INGOT.getMaterialIngredient(Tin, 1)).io(INGOT.get(Bronze, 3)).add("bronze_ingot_from_tetra", 150, 16);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), INGOT.getMaterialIngredient(Zinc, 1)).io(INGOT.get(Brass, 3)).add("brass_ingot_from_tetra",150, 16);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), DUST.getMaterialIngredient(Tin, 1)).io(INGOT.get(Bronze, 3)).add("bronze_ingot_from_tetra_1",150, 16);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), DUST.getMaterialIngredient(Zinc, 1)).io(INGOT.get(Brass, 3)).add("brass_ingot_from_tetra_1",150, 16);
-        addAlloyRecipes(Copper, 3, Tin, 1, Bronze, 4, 200);
-        addAlloyRecipes(Copper, 3, Zinc, 1, Brass, 4, 200);
-        addAlloyRecipes(Copper, 1, Nickel, 1, Cupronickel, 2, 100);
-        addAlloyRecipes(Iron, 2, Nickel, 1, Invar, 3, 150);
-        addAlloyRecipes(Tin, 9, Antimony, 1, SolderingAlloy, 10, 500);
-        addAlloyRecipes(Lead, 4, Antimony, 1, BatteryAlloy, 5, 250);
-        addAlloyRecipes(AntimatterMaterials.Gold, 1, Silver, 1, Electrum, 2, 100);
-        addAlloyRecipes(Magnesium, 1, Aluminium, 2, Magnalium, 3, 150);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(Redstone, 4), DUST.getMaterialIngredient(Copper, 1)).io(INGOT.get(RedAlloy, 1)).add("red_alloy_ingot", 50, 16);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(Redstone, 4), INGOT.getMaterialIngredient(Copper, 1)).io(INGOT.get(RedAlloy, 1)).add("red_alloy_ingot_1", 50, 16);
+        addAlloyRecipes(ImmutableMap.of(Tetrahedrite, 3, Tin, 1), Bronze, 3, "bronze_ingot_from_tetrahedrite");
+        addAlloyRecipes(ImmutableMap.of(Tetrahedrite, 3, Zinc, 1), Brass, 3, "brass_ingot_from_tetrahedrite");
+        addAlloyRecipes(ImmutableMap.of(Copper, 3, Tin, 1), Bronze);
+        addAlloyRecipes(ImmutableMap.of(Copper, 3, Zinc, 1), Brass);
+        addAlloyRecipes(ImmutableMap.of(Copper, 1, Nickel, 1), Cupronickel);
+        addAlloyRecipes(ImmutableMap.of(Iron, 2, Nickel, 1), Invar);
+        addAlloyRecipes(ImmutableMap.of(Tin, 9, Antimony, 1), SolderingAlloy);
+        addAlloyRecipes(ImmutableMap.of(Lead, 4, Antimony, 1), BatteryAlloy);
+        addAlloyRecipes(ImmutableMap.of(Gold, 1, Silver, 1), Electrum);
+        addAlloyRecipes(ImmutableMap.of(Magnesium, 1, Aluminium, 2), Magnalium);
+        addAlloyRecipes(ImmutableMap.of(Copper, 1, Redstone, 4), RedAlloy, 1);
+        addAlloyRecipes(ImmutableMap.of(Lead, 1, Redstone, 4), GTCoreMaterials.LeadedRedstone, 1);
         //TODO compat for bluepower
         //ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Nikolite, 4), DUST.getMaterialIngredient(Copper)).io(INGOT.get(BlueAlloy, 1)).add(50, 16);
         //ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Nikolite, 4), INGOT.getMaterialIngredient(Copper)).io(INGOT.get(BlueAlloy, 1)).add(50, 16);
@@ -45,10 +51,28 @@ public class AlloySmelterLoader {
 
     }
 
-    private static void addAlloyRecipes(Material input1, int count1, Material input2, int count2, Material output, int countO, int duration){
-        ALLOY_SMELTER.RB().ii(INGOT.getMaterialIngredient(input1, count1), INGOT.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot", duration, 16);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(input1, count1), INGOT.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot_1", duration, 16);
-        ALLOY_SMELTER.RB().ii(INGOT.getMaterialIngredient(input1, count1), DUST.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot_2", duration, 16);
-        ALLOY_SMELTER.RB().ii(DUST.getMaterialIngredient(input1, count1), DUST.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot_3", duration, 16);
+    public static void addAlloyRecipes(ImmutableMap<Material, Integer> inputs, Material output){
+        addAlloyRecipes(inputs, output, inputs.values().stream().mapToInt(i -> i).sum(), output.getId() + "_ingot");
+    }
+
+    public static void addAlloyRecipes(ImmutableMap<Material, Integer> inputs, Material output, int amount){
+        addAlloyRecipes(inputs, output, amount, output.getId() + "_ingot");
+    }
+
+    public static void addAlloyRecipes(ImmutableMap<Material, Integer> inputs, Material output, int amount, String id){
+        if (inputs.size() > 1){
+            List<Ingredient> ingredients = new ArrayList<>();
+            inputs.forEach((m, i) -> {
+                List<TagKey<Item>> tags = new ArrayList<>();
+                if (m.has(DUST)){
+                    tags.add(DUST.getMaterialTag(m));
+                }
+                if (m.has(INGOT)) {
+                    tags.add(INGOT.getMaterialTag(m));
+                }
+                ingredients.add(RecipeIngredient.of(i, tags.toArray(TagKey[]::new)));
+            });
+            ALLOY_SMELTER.RB().ii(ingredients).io(INGOT.get(output, amount)).add(id, 50L * amount, 16);
+        }
     }
 }
